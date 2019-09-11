3558cd49ec90d428d2c786ac7ecf7628c1140878
star008 <star008@protonmail.com>
2019-09-11 17:35:32 +0300

    Clean up the source

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
-    apiKey: "AIzaSyB7OKvbx-NWQcM70T4dEnoTDt3aAeEkJ2A",
-    authDomain: "antqueuetest-ce4e9.firebaseapp.com",
-    databaseURL: "https://antqueuetest-ce4e9.firebaseio.com",
-    projectId: "antqueuetest-ce4e9",
-    storageBucket: "antqueuetest-ce4e9.appspot.com",
-    messagingSenderId: "341478783416",
-    appId: "1:341478783416:web:37313bb2057c0a99"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 
 export default config;
