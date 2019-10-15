a34e3c50d366a1200af6c714faf7fb4faebfadc1
star008 <star008@protonmail.com>
2019-10-15 16:41:31 +0300

    Modify info file

--- src/info.js
+++ src/info.js
@@ -1,12 +1,14 @@
 const info = {
-    app_name: "My 230620191.0 Dev1",
+    app_name: "Ant Queue",
     user: "",
     pass: "",
     loginUrl: "",
     changePassUrl: "",
     site_key: "",
     google_client_id: "",
-    facebook_app_id: ""
+    facebook_app_id: "",
+    ip_api_key: "",
+    google_map_api_key: ""
 };
 
 export default info;
